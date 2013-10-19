406c5ce7d4065f60978ae51b0c2046519fb8f2f6
joshaber <joshaber@gmail.com>
2013-10-19 01:02:49 -0400

    Instrument some writes.

--- DatBase/DABAppDelegate.m
+++ DatBase/DABAppDelegate.m
@@ -15,38 +15,59 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
 	NSError *error;
-	DABCoordinator *coordinator = [DABCoordinator createDatabaseAtURL:[NSURL fileURLWithPath:@"/Users/joshaber/Desktop/DatBase/test.git"] error:&error];
+	NSURL *URL = [NSURL fileURLWithPath:@"/Users/joshaber/Desktop/DatBase/test.git"];
+	[NSFileManager.defaultManager removeItemAtURL:URL error:NULL];
+
+	DABCoordinator *coordinator = [DABCoordinator createDatabaseAtURL:URL error:&error];
 	NSAssert(coordinator != nil, @"Coordinator was nil: %@", error);
 
-	DABDatabase *originalDatabase = [coordinator currentDatabase:&error];
-	NSAssert(originalDatabase != nil, @"Original database was nil: %@", error);
-
-	static NSString * const attribute = @"answer";
-	NSNumber *answer = @42;
 	DABTransactor *transactor = [coordinator transactor];
-	NSString *newKey = [transactor generateNewKey];
-	[transactor runTransaction:^{
-		NSError *error;
-		NSString *key = [transactor addValue:@43 forAttribute:attribute key:newKey error:&error];
-		NSAssert(key != nil, @"Key was nil: %@", error);
 
-		key = [transactor addValue:answer forAttribute:attribute key:newKey error:&error];
-		NSAssert(key != nil, @"Key was nil: %@", error);
+	[self doABunchOfWrites:transactor];
 
-	}];
+//	DABDatabase *originalDatabase = [coordinator currentDatabase:&error];
+//	NSAssert(originalDatabase != nil, @"Original database was nil: %@", error);
+//
+//	static NSString * const attribute = @"answer";
+//	NSNumber *answer = @42;
+//	DABTransactor *transactor = [coordinator transactor];
+//	NSString *newKey = [transactor generateNewKey];
+//	[transactor runTransaction:^{
+//		NSError *error;
+//		NSString *key = [transactor addValue:@43 forAttribute:attribute key:newKey error:&error];
+//		NSAssert(key != nil, @"Key was nil: %@", error);
+//
+//		key = [transactor addValue:answer forAttribute:attribute key:newKey error:&error];
+//		NSAssert(key != nil, @"Key was nil: %@", error);
+//
+//	}];
+//
+//	DABDatabase *database = [coordinator currentDatabase:&error];
+//	NSAssert(database != nil, @"Database was nil: %@", error);
+//
+//	NSDictionary *values = database[newKey];
+//	NSString *value = values[attribute];
+//	NSAssert([value isEqual:answer], @"Value was not \"%@\": %@", answer, value);
+//
+//	values = originalDatabase[newKey];
+//	value = values[attribute];
+//	NSAssert(![value isEqual:answer], @"Found the updated value even though we shouldn't see it :(");
+//
+//	NSLog(@"%@", database.allKeys);
+}
 
-	DABDatabase *database = [coordinator currentDatabase:&error];
-	NSAssert(database != nil, @"Database was nil: %@", error);
+- (void)doABunchOfWrites:(DABTransactor *)transactor {
+	NSTimeInterval start = [NSDate timeIntervalSinceReferenceDate];
 
-	NSDictionary *values = database[newKey];
-	NSString *value = values[attribute];
-	NSAssert([value isEqual:answer], @"Value was not \"%@\": %@", answer, value);
+	static const NSUInteger count = 1000;
+	static NSString * const attribute = @"attribute";
+	for (NSUInteger i = 0; i < count; i++) {
+		NSString *newKey = [transactor generateNewKey];
+		[transactor addValue:@(i) forAttribute:attribute key:newKey error:NULL];
+	}
 
-	values = originalDatabase[newKey];
-	value = values[attribute];
-	NSAssert(![value isEqual:answer], @"Found the updated value even though we shouldn't see it :(");
-
-	NSLog(@"%@", database.allKeys);
+	NSTimeInterval end = [NSDate timeIntervalSinceReferenceDate];
+	NSLog(@"Took %f secs.", end - start);
 }
 
 @end
