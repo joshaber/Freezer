e0c5e2c5f73445c24cfe5d054139cd8dedc833c8
joshaber <joshaber@gmail.com>
2013-12-27 17:50:04 -0500

    Test performance too.

--- FRZDemoApp/FRZDemoApp/FDAAppDelegate.m
+++ FRZDemoApp/FRZDemoApp/FDAAppDelegate.m
@@ -17,8 +17,13 @@
 @implementation FDAAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
-	_store = [[FRZStore alloc] initInMemory:NULL];
+	NSString *path = [NSTemporaryDirectory() stringByAppendingPathComponent:[[NSUUID UUID] UUIDString]];
+	_store = [[FRZStore alloc] initWithURL:[NSURL fileURLWithPath:path] error:NULL];
 
+	[self testPerformance];
+}
+
+- (void)testStuff {
 	static NSString * const firstNameAttribute = @"user/first-name";
 	static NSString * const lastNameAttribute = @"user/last-name";
 	static NSString * const hubbersAttribute = @"user/hubbers";
@@ -26,16 +31,16 @@
 	FRZTransactor *transactor = [self.store transactor];
 	NSString *hubbersKey = [transactor generateNewKey];
 	[[[self.store.changes
-		filter:^ BOOL (FRZChange *change) {
-			return change.type == FRZChangeTypeAdd && [change.key isEqual:hubbersKey];
-		}]
-		map:^(FRZChange *change) {
-			NSString *keyInserted = change.delta;
-			return change.changedDatabase[keyInserted];
-		}]
-		subscribeNext:^(NSDictionary *x) {
-			NSLog(@"%@ is a GitHubber!", x[firstNameAttribute]);
-		}];
+	   filter:^ BOOL (FRZChange *change) {
+		   return change.type == FRZChangeTypeAdd && [change.key isEqual:hubbersKey];
+	   }]
+	  map:^(FRZChange *change) {
+		  NSString *keyInserted = change.delta;
+		  return change.changedDatabase[keyInserted];
+	  }]
+	 subscribeNext:^(NSDictionary *x) {
+		 NSLog(@"%@ is a GitHubber!", x[firstNameAttribute]);
+	 }];
 
 	[transactor addAttribute:firstNameAttribute type:FRZAttributeTypeString collection:NO error:NULL];
 	[transactor addAttribute:lastNameAttribute type:FRZAttributeTypeString collection:NO error:NULL];
@@ -81,6 +86,46 @@
 	}];
 
 	[transactor addValue:jssKey forAttribute:hubbersAttribute key:hubbersKey error:NULL];
+}
+
+- (void)testPerformance {
+	static NSString * const testAttribute = @"testAttribute";
+	FRZTransactor *transactor = [self.store transactor];
+	[transactor addAttribute:testAttribute type:FRZAttributeTypeInteger collection:NO error:NULL];
+
+	NSTimeInterval start = [NSDate timeIntervalSinceReferenceDate];
+	__block NSUInteger writes = 0;
+	[transactor performChangesWithError:NULL block:^(NSError **error) {
+		while (YES) {
+			if ([NSDate timeIntervalSinceReferenceDate] - start > 5) break;
+
+			[transactor addValue:@42 forAttribute:testAttribute key:[transactor generateNewKey] error:NULL];
+
+			writes++;
+		}
+
+		return YES;
+	}];
+
+	NSLog(@"Writes/sec: %lu", writes);
+
+	FRZDatabase *database = [self.store currentDatabase];
+	NSString *key = [self.store currentDatabase].allKeys.anyObject;
+	start = [NSDate timeIntervalSinceReferenceDate];
+	__block NSUInteger reads = 0;
+	[transactor performChangesWithError:NULL block:^(NSError **error) {
+		while (YES) {
+			if ([NSDate timeIntervalSinceReferenceDate] - start > 5) break;
+
+			id value __unused = database[key];
+
+			reads++;
+		}
+
+		return YES;
+	}];
+
+	NSLog(@"Reads/sec: %lu", reads);
 }
 
 @end
